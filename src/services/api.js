ba5dd6664a52d7f03dbccb953ffedbe15b86fbfe
iGWE-eDWIN <igweedwin15@gmail.com>
2025-10-21 18:20:21 +0100

    api-service

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL || 'https://edukasterserver.onrender.com';
 
 class ApiService {
   constructor() {
